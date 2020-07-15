36688ce86e93245aa8d4b133813ab3f56abcf63d
Akeem Flavors <akeemflavors@gmail.com>
2020-07-14 22:23:04 -0400

    fixed path

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
     });
 
     app.get("/", function(req, res) {
-        res.json(path.join(__dirname, "public/index.html"));
+        res.json(path.join(__dirname, "../public/index.html"));
     });
       
 };
