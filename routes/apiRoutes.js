fb0afc9a75496fc997e3ddab8fc74e5767e7638c
Akeem Flavors <akeemflavors@gmail.com>
2020-07-14 22:06:13 -0400

    uuidv4 git status

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@
 // ==============================================================================
 var path = require("path");
 var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -19,8 +20,24 @@ app.get("/api/notes", function(req, res) {
     });
   
   });
-
 //POST /api/notes add new note to db.json
+app.post("/api/notes", function(req, res){
+    let note = (req.body)
+    let id = uuidv4(); 
+    note.id = id
+    let notesArray;
+    let savedNotes = fs.readFileSync("./db/db.json","utf-8");
+    notesArray = JSON.parse(savedNotes);
+    notesArray.push(note);
+    
+    fs.writeFile("./db/db.json", JSON.stringify(notesArray), function(error){
+        if (error) {
+        console.log(error);
+        }
+        console.log(notesArray); 
+        return res.json(notesArray); 
+    });
+})
 
 //DELETE /api/notes/:id from db.json
 };
