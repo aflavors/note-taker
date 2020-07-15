ccac7aeba440109c3bd77f6e4d147fffa2bcf154
Akeem Flavors <akeemflavors@gmail.com>
2020-07-14 21:49:47 -0400

    PORT 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { v4: uuidv4 } = require('uuid');
 // ==============================================================================
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
