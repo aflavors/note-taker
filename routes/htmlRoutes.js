742f061019fac16806343742c408444377cd660f
Akeem Flavors <akeemflavors@gmail.com>
2020-07-14 22:22:45 -0400

    / route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,5 +15,10 @@ module.exports = function(app) {
 
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
-      });
+    });
+
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/index.html"));
+    });
+      
 };
