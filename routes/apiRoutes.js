992f8c99051a792198535320958a539edb441739
Akeem Flavors <akeemflavors@gmail.com>
2020-07-14 21:56:57 -0400

    app.get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,16 @@ var fs = require("fs");
 
 module.exports = function(app) {
 //GET /api/notes to read db.json
+app.get("/api/notes", function(req, res) {
+
+    fs.readFile("./db/db.json", "utf-8", function(error, data){
+      if (error) {
+        return console.log(error);
+      }
+      return res.json(JSON.parse(data));
+    });
+  
+  });
 
 //POST /api/notes add new note to db.json
 
